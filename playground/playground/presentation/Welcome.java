8527cb47dd51bebeada5a25eab105900e1f97af3
locher <locher>
2000-04-12 16:53:58 +0000

    changed welcome page

--- playground/playground/presentation/Welcome.java
+++ playground/playground/presentation/Welcome.java
@@ -6,13 +6,11 @@ import com.lutris.appserver.server.httpPresentation.*;
 
 public class Welcome implements HttpPresentation {
 
-
     public void run(HttpPresentationComms comms) 
         throws HttpPresentationException {
 
         String now = new Date().toString();
         WelcomeHTML welcome = new WelcomeHTML();
-        welcome.setTextTime(now);
         comms.response.writeHTML(welcome.toDocument());
     }
 
