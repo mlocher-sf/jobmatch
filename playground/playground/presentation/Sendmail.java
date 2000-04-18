f23dacd767af45e35235816667883344be676b58
locher <locher>
2000-04-18 13:06:12 +0000

    debug prints

--- playground/playground/presentation/Sendmail.java
+++ playground/playground/presentation/Sendmail.java
@@ -17,10 +17,17 @@ public class Sendmail implements HttpPresentation {
   	String msg = comms.request.getParameter("Message");
 
 	String host = "mail.unibe.ch";
+
+	System.out.println(comms.request.getParameter("From"));
+  	System.out.println(comms.request.getParameter("To"));
+  	System.out.println(comms.request.getParameter("Subject"));
+  	System.out.println(comms.request.getParameter("Message"));
 	
 	try {
 	    Mailer.sendMail(host, from, to, subject, msg);
-	} catch (Exception e) {}
+	} catch (Exception e) {
+	    System.err.println("Mailing Error:\n"+e.toString());
+	}
 
 	// redirect to static page
 	throw new ClientPageRedirectException(
