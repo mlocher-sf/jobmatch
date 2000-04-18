88726a0dbb3b11dda62804c55fc071867a171183
locher <locher>
2000-04-18 13:13:46 +0000

    working mailer

--- playground/playground/presentation/Sendmail.java
+++ playground/playground/presentation/Sendmail.java
@@ -11,18 +11,13 @@ public class Sendmail implements HttpPresentation {
     public void run(HttpPresentationComms comms) 
         throws HttpPresentationException {
 
-  	String from = comms.request.getParameter("From");
-  	String to = comms.request.getParameter("To");
-  	String subject = comms.request.getParameter("Subject");
-  	String msg = comms.request.getParameter("Message");
+	final String from = comms.request.getParameter("From");
+  	final String to = comms.request.getParameter("To");
+  	final String subject = comms.request.getParameter("Subject");
+  	final String msg = comms.request.getParameter("Message");
 
-	String host = "mail.unibe.ch";
+	final String host = "mail.unibe.ch";
 
-	System.out.println(comms.request.getParameter("From"));
-  	System.out.println(comms.request.getParameter("To"));
-  	System.out.println(comms.request.getParameter("Subject"));
-  	System.out.println(comms.request.getParameter("Message"));
-	
 	try {
 	    Mailer.sendMail(host, from, to, subject, msg);
 	} catch (Exception e) {
