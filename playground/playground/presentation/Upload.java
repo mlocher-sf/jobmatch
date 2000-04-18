2bee88eaf2ddb291f8e7020ffa2f6db5e68277a6
locher <locher>
2000-04-18 12:12:45 +0000

    worked on upload

--- playground/playground/presentation/Upload.java
+++ playground/playground/presentation/Upload.java
@@ -2,7 +2,10 @@ package playground.presentation;
 
 import playground.business.*;
 
+import java.io.*;
+import gnu.regexp.*;
 import org.w3c.dom.html.*;
+import com.lutris.mime.*;
 import com.lutris.xml.xmlc.*;
 import com.lutris.appserver.server.httpPresentation.*;
 
@@ -13,8 +16,35 @@ public class Upload implements HttpPresentation {
 
 	PictureHTML page = new PictureHTML();
 
-	System.out.println(comms.request.toString());
+ 	String event = comms.request.getParameter("event");
+ 	System.out.println(comms.request.getHttpServletRequest().getMethod().toUpperCase());
+
+	if (comms.request.getHttpServletRequest().getMethod().toUpperCase().equals("POST")) {
+	    HttpPresentationInputStream in = comms.request.getInputStream();
+	    this.save(in);
+	}
     	
         comms.response.writeHTML(page.toDocument());
+    }
+
+    private void save(HttpPresentationInputStream in) {
+	try {
+	    String RE = "(?:Content-Disposition: form-data; name=\"image\"; filename=\")";
+
+	    System.out.println(RE);
+	    System.out.println(new RE(RE));
+
+
+	    OutputStream out = new FileOutputStream("output.txt");
+	    
+	    byte[] buffer = new byte[512];
+	    int offset = 0;
+	    while( 0 != in.available()) {
+		int numRead = in.readLine(buffer, 0, 512);
+		System.out.print(new String(buffer, 0, numRead));
+		out.write(buffer, 0, numRead);
+	    }
+	    out.flush();
+	} catch (Exception e) {}
     }
 }
