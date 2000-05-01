46fa5f1db4c11ccc614b953f16ef4713a0e3561c
studer <studer>
2000-05-01 12:10:55 +0000

    Exception geaendert

--- playground2/playground/business/InsertData.java
+++ playground2/playground/business/InsertData.java
@@ -1,19 +1,22 @@
 package playground.business;
-import com.lutris.appserver.server.sql.DatabaseManagerException;
-import com.lutris.appserver.server.sql.ObjectIdException;
-//import com.lutris.dods.builder.generator.query.DataObjectException;
+//import com.lutris.appserver.server.sql.DatabaseManagerException;
+//import com.lutris.appserver.server.sql.ObjectIdException;
+import com.lutris.dods.builder.generator.query.DataObjectException;
 import playground.data.*;
 
 public class InsertData{
     public static StudentsDO studentsDO = null;
     
-    public static void insert(){
+    public static void insert() throws DataObjectException{
         try {
             studentsDO = StudentsDO.createVirgin();  
 	    studentsDO.setName("mario");
 	    studentsDO.setLname("loeffel");
 	    studentsDO.commit();
-        } catch(Exception ex) {ex.printStackTrace();}
+        } catch(Exception ex) {
+	    String message = "Insertion into DB failed: "+ ex.getMessage(); 
+            throw new DataObjectException(message);
+		}
      }
  }
 
