c2ddc8ae69275ba512a25facaafefdc022a52496
locher <locher>
2000-05-30 10:30:10 +0000

    also create accounts

--- pse/kg2k/src/jobmatch/scripts/candidate.py
+++ pse/kg2k/src/jobmatch/scripts/candidate.py
@@ -1,14 +1,32 @@
 import jobmatch
-from jobmatch.business.candidate import Candidate
 
+accountManager = jobmatch.business.provider.account.AccountManager.getUniqueInstance()
 
 candidates = [
-    ("Muster", "Hans"),
-    ("Bruhin", "Michel")]
+    ("muster", "muster", "muster@mueller.ch", "Muster", "Hans"),
+    ("bruhin", "bruhin", "bruhin@iamexwi.unibe.ch", "Bruhin", "Michel"),
+    ("pater", "noster", "pater@divine.ch", "Pope", "Sepp")
+    ]
 
-for (lastname, firstname) in candidates:
-    c = Candidate()
-    c.setLname(lastname)
-    c.setFname(firstname)
-    c.commit()
+for (login, passwd, email, lastname, firstname) in candidates:
+    try:
+        accountManager.createCandidateAccount(login, passwd, email)
+    except:
+        print 'account %s exists already' % login
+        account = accountManager.getCandidateAccount(login)
+        account.setPassphrase(passwd)
+        account.setEmail(email)
+        account.commit()
+        
+    account = accountManager.getCandidateAccount(login)
+    
+    candidate = account.getCandidateBO()
+    candidate.setLname(lastname)
+    candidate.setFname(firstname)
+    candidate.setStatus(1)
+    candidate.commit()
+
+    print '-' * 50
+    print str(account)
+
     
