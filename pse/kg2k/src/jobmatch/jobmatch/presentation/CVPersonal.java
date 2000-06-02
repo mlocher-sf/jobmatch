08772c0d11ab5b655af27e1993255fba828a75f8
loeffel <loeffel>
2000-06-02 14:21:42 +0000

    Reading Data from HTML and writing to DB (and vice versa) works.

--- pse/kg2k/src/jobmatch/jobmatch/presentation/CVPersonal.java
+++ pse/kg2k/src/jobmatch/jobmatch/presentation/CVPersonal.java
@@ -7,6 +7,7 @@ import java.util.List;
 import jobmatch.business.entity.*;
 import com.lutris.xml.xmlc.*;
 import com.lutris.appserver.server.httpPresentation.*;
+import jobmatch.business.util.TimeUtil;
 
 public class CVPersonal extends CVSection implements HttpPresentation {
 
@@ -16,16 +17,53 @@ public class CVPersonal extends CVSection implements HttpPresentation {
 	Candidate candidate = account.getCandidateBO();
         CVPersonalHTML page = (CVPersonalHTML)comms.xmlcFactory.create(CVPersonalHTML.class);
 	fillNationality(page, candidate);
+<<<<<<< CVPersonal.java
+	final String action = comms.request.getParameter("action");
+	System.out.println("Die Action lautet: " + action);
+	writeTextFields(page, candidate, comms);
+	if (action != null && action.equals("write")){ 
+	    readTextFields(page, candidate, comms);
+	}
+	comms.response.writeHTML(page);
+=======
 	final String action = comms.request.getParameter("action");
 	if (action != null && action.equals("write")){ 
 	    fillTextFields(page, candidate, comms);
 	}
 	comms.response.writeHTML(page);
+>>>>>>> 1.5
     }
 
     /**
      * Fills the text fields with CV-data 
      **/
+<<<<<<< CVPersonal.java
+    private void readTextFields(CVPersonalHTML page, Candidate candidate, HttpPresentationComms comms){
+	try{
+	    candidate.setLname(comms.request.getParameter("lName"));
+	    candidate.setFname(comms.request.getParameter("fName"));
+	    candidate.setSex(comms.request.getParameter("sex"));
+	    candidate.setBirthdate(TimeUtil.getDate(Integer.parseInt(comms.request.getParameter("year")),
+						    Integer.parseInt(comms.request.getParameter("month"))-1,
+						    Integer.parseInt(comms.request.getParameter("day"))));
+	    candidate.setResidence(comms.request.getParameter("residence"));
+	    //candidate.setNationality(comms.request.getParameter("nationality"));
+	    candidate.setStreet(comms.request.getParameter("street"));
+	    candidate.setHouseNumber(comms.request.getParameter("housenumber"));
+	    candidate.setPLZ(Integer.parseInt(comms.request.getParameter("zip")));
+	    candidate.setCity(comms.request.getParameter("city"));
+	    //candidate.setCountry(comms.request.getParameter("country"));
+	    candidate.setPhone(comms.request.getParameter("phonenumber"));
+	    candidate.setFax(comms.request.getParameter("faxnumber"));
+	    candidate.setNatel(comms.request.getParameter("natel"));
+	    //candidate.setPicture(comms.request.getParameter("picture")); 
+
+	    candidate.commit();
+	}
+	catch(Exception e){
+	    System.out.println(e.toString());
+	    throw new RuntimeException();}
+=======
     private void fillTextFields(CVPersonalHTML page, Candidate candidate, HttpPresentationComms comms){
 	try{
 	    candidate.setLname(comms.request.getParameter("lastName"));
@@ -35,7 +73,34 @@ public class CVPersonal extends CVSection implements HttpPresentation {
 	catch(Exception e){
 	    System.out.println(e.toString());
 	    throw new RuntimeException();}
+>>>>>>> 1.5
     }
+
+    private void writeTextFields(CVPersonalHTML page, Candidate candidate, HttpPresentationComms comms){
+	try{
+
+	    page.getElementLName().setValue(candidate.getLname());
+	    page.getElementFName().setValue(candidate.getFname());
+	  //  page.getElementSex().setValue(candidate.getSex());
+	  //  page.getElementYear().setValue(candidate.getBirthdate());
+	    page.getElementResidence().setValue(candidate.getResidence());
+	    page.getElementStreet().setValue(candidate.getStreet());
+	    page.getElementHousenumber().setValue(candidate.getHouseNumber());
+	    page.getElementZip().setValue(String.valueOf(candidate.getPLZ()));
+	    page.getElementCity().setValue(candidate.getCity());
+	    page.getElementPhonenumber().setValue(candidate.getPhone());
+	    page.getElementFaxnumber().setValue(candidate.getFax());
+	    page.getElementNatel().setValue(candidate.getNatel());
+	    comms.response.writeHTML(page);
+    
+	}
+	catch(Exception e){
+	    System.out.println(e.toString());
+	    throw new RuntimeException();}
+	    
+	}
+	
+    
 
     /**
      * fills the nationality-list with CV-data
