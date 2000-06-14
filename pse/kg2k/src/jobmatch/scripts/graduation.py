b86e25be694accd412a62411150ac0b15e8c9e4f
studer <studer>
2000-06-14 07:42:22 +0000

    *** empty log message ***

---
+++ pse/kg2k/src/jobmatch/scripts/graduation.py
@@ -0,0 +1,24 @@
+import jobmatch
+
+Graduation = jobmatch.business.entity.Graduation
+
+graduations = [
+    ("Matur"),
+    ("Berufsmatur"),
+    ("Primarlehrerpatent")
+  
+    ]
+
+for (name) in graduations:
+    
+    grad = Graduation.getGraduation(name)
+  
+  
+    
+    
+                            
+    
+
+    
+
+    
