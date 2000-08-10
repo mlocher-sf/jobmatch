085f5f59a5c692ac165f47bfa7bf43814af7cedb
mlocher <mlocher>
2000-08-10 21:19:19 +0000

    removed useless stuff

--- playground2/playground/presentation/Upload.java
+++ playground2/playground/presentation/Upload.java
@@ -17,11 +17,7 @@ public class Upload implements HttpPresentation {
 
 	PictureHTML page = (PictureHTML)comms.xmlcFactory.create(PictureHTML.class);
 
- 	String event = comms.request.getParameter("event");
-// 	System.out.println(comms.request.getHttpServletRequest().getMethod().toUpperCase());
-
 	if (comms.request.getHttpServletRequest().getMethod().toUpperCase().equals("POST")) {
-	    HttpPresentationInputStream in = comms.request.getInputStream();
 	    this.save(comms);
 	}
     	
@@ -34,34 +30,10 @@ public class Upload implements HttpPresentation {
 	    final int MAX_SIZE = 256 * 1024;
 	    MultipartRequest multi = new MultipartRequest(comms.request.getHttpServletRequest(), "/tmp", MAX_SIZE); 
 
-/*	    System.out.println("Params:"); 
-	    Enumeration params = multi.getParameterNames(); 
-	    while (params.hasMoreElements()) { 
-		String name = (String)params.nextElement(); 
-		String value = multi.getParameter(name);
-		System.out.println(name + " = " + value);
-	    }
-	    System.out.println();
-	    System.out.println("Files:");*/
-
 	    Enumeration files = multi.getFileNames();
 	    while (files.hasMoreElements()) {
 		String name = (String)files.nextElement();
-//		String filename = multi.getFilesystemName(name);
-//		String type = multi.getContentType(name);
 		File f = multi.getFile(name);
-
-/*		System.out.println("name: " + name);
-		System.out.println("filename: " + filename);
-		System.out.println("type: " + type);
-		
-		if (f != null) {
-		    System.out.println("f.toString(): " + f.toString());
-		    System.out.println("f.getName(): " + f.getName());
-		    System.out.println("f.exists(): " + f.exists());
-		    System.out.println("f.length(): " + f.length());
-		    System.out.println(); 
-		} */
 	    }
 	} catch (Exception e) {System.err.println(e);}
     }
